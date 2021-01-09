18f1f89e967acfccac8d74f21f204b22c96e9710
Econa77 <s.f.1992.ip@gmail.com>
2021-01-09 16:40:54 +0900

    Add demo codes

---
+++ Demo/CustomDesignCollectionViewCell.swift
@@ -0,0 +1,10 @@
+//
+//  CustomDesignCollectionViewCell.swift
+//  Demo
+//
+//  Created by 古林俊佑 on 2021/01/09.
+//
+
+import UIKit
+
+final class CustomDesignCollectionViewCell: UICollectionViewCell {}
--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import FSPagerView
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    // MARK: - Properties
+    @IBOutlet private weak var pagerView: FSPagerView! {
+        didSet {
+            pagerView.register(UINib(nibName: "CustomDesignCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "CustomDesignCollectionViewCell")
+            pagerView.dataSource = self
+            pagerView.interitemSpacing = 10
+        }
     }
 
+    // MARK: - View Life Cycle
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        pagerView.itemSize = CGSize(width: pagerView.frame.width - 30, height: pagerView.frame.height)
+    }
 
 }
 
+// MARK: - FSPagerViewDataSource
+extension ViewController: FSPagerViewDataSource {
+    func numberOfItems(in pagerView: FSPagerView) -> Int {
+        return 5
+    }
+
+    func pagerView(_ pagerView: FSPagerView, cellForItemAt index: Int) -> UICollectionViewCell {
+        let cell = pagerView.dequeueReusableCell(withReuseIdentifier: "CustomDesignCollectionViewCell", at: index)
+        // If you access the cell, please cast it here
+        // if let cell = cell as? CustomDesignCollectionViewCell {}
+        return cell
+    }
+}
