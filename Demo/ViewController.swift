7fc0d6b064932eb1beb7e7f209d10fde04a5b255
Econa77 <s.f.1992.ip@gmail.com>
2021-01-09 16:32:05 +0900

    Add template project

---
+++ Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by 古林俊佑 on 2021/01/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
